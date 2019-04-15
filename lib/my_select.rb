8fee9396559f4a76363236521a66c7c9268032bb
Iskra B <ibatistapoblete@gmail.com>
2019-04-15 05:42:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   new_array = []
   while i < array.length
-    new_array.push yield(array.select[i])
+    new_array.push if yield([i])
     i+=1
   end
   new_array
